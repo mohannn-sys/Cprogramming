d55df5a2a71b3d7af0fe1d46a6a8b114767ce47f
Mohn Kummr <54698755+mohannn-sys@users.noreply.github.com>
2024-01-11 14:11:48 +0000

    Update inlineFunctions.c

--- inlineFunctions.c
+++ inlineFunctions.c
@@ -1,5 +1,5 @@
-/*The main advantage of using inline functions is that it can lead to potentially faster code execution by avoiding the overhead associated with a regular function call. Instead of jumping to a separate function, the code of the inline function is inserted directly at the point where the function is called, reducing the function call overhead./*
-n inline function is a function that is expanded in place at the point where it is called, rather than being called through a regular function call mechanism.
+/*
+An inline function is a function that is expanded in place at the point where it is called, rather than being called through a regular function call mechanism.
 The inline keyword is used to indicate to the compiler that a particular function should be treated as inline.
 The main advantage of using inline functions is that it can lead to potentially faster code execution by avoiding the overhead associated with a
 regular function call. Instead of jumping to a separate function, the code of the inline function is inserted directly at the point where the
